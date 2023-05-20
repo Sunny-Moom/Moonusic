338b9adb7d4c422cb5b914a63bfbba5fe53f9145
Sunny-Moom <khgkhh@foxmail.com>
2023-05-21 02:50:12 +0800

    updata

--- app/src/main/java/com/startfly/moonusic/HomeActivity.kt
+++ app/src/main/java/com/startfly/moonusic/HomeActivity.kt
@@ -5,15 +5,14 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
-    val token = intent.getStringExtra("token")
-    val name = intent.getStringExtra("name")
-    val subsonicSalt = intent.getStringExtra("subsonicSalt")
-    val subsonicToken = intent.getStringExtra("subsonicToken")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val token = intent.getStringExtra("token")
         val name = intent.getStringExtra("name")
+        val subsonicSalt = intent.getStringExtra("subsonicSalt")
+        val subsonicToken = intent.getStringExtra("subsonicToken")
         val textView = findViewById<TextView>(R.id.token) // 替换 R.id.my_text_view 为你的 TextView 的 id
-        textView.text = name
+        textView.text = token
     }
 }
