c4dc082a25879fe0e90867db42215f0740ba5e37
Sunny-Moom <khgkhh@foxmail.com>
2023-05-22 10:11:10 +0800

    updata

--- app/src/main/java/com/startfly/moonusic/HomeActivity.kt
+++ app/src/main/java/com/startfly/moonusic/HomeActivity.kt
@@ -1,7 +1,6 @@
 package com.startfly.moonusic
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
@@ -12,7 +11,5 @@ class HomeActivity : AppCompatActivity() {
         val name = intent.getStringExtra("name")
         val subsonicSalt = intent.getStringExtra("subsonicSalt")
         val subsonicToken = intent.getStringExtra("subsonicToken")
-        val textView = findViewById<TextView>(R.id.token) // 替换 R.id.my_text_view 为你的 TextView 的 id
-        textView.text = token
     }
 }
